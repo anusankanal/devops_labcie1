c3abe81109be207583bf259d6d567cf5067a2231
Anusha S <anusankanal@gmail.com>
2025-10-18 10:13:20 +0530

    save

---
+++ devops_labcie1.py
@@ -0,0 +1,11 @@
+temp=int(input("Enter the temperaturein celsius:"))
+
+if temp<20:
+    status:"cold"
+elif temp 20< temp <30:
+    status:"Normal"
+else
+    status:"hot"
+    
+print("temperature,{temp}celsius")
+print("status:"{status})
